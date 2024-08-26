1c21d9cc4ff74d661055e44609947a266349f85b
Cédric <atorionebula@gmail.com>
2024-08-26 16:01:45 +0200

    Initial Commit

---
+++ Projet_Mobile_Swift/Projet_Mobile_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Projet_Mobile_SwiftApp.swift
+//  Projet_Mobile_Swift
+//
+//  Created by Cédric Galais on 26/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Projet_Mobile_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
